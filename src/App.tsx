63d9bb283976abba40117b59a18fb2cde7f15e8d
LAPTOP-1A6BH9TL\Lee Yi <leeyi45@gmail.com>
2026-08-25 12:38:42 +0800

    Remove the change detection cause it wasn't working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import CloseIcon from '@mui/icons-material/Close';
 import DeleteIcon from '@mui/icons-material/Delete';
-import DoneIcon from '@mui/icons-material/Done';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import SaveIcon from '@mui/icons-material/Save';
 import Autocomplete from '@mui/material/Autocomplete';
-import Backdrop from '@mui/material/Backdrop';
-import Badge from '@mui/material/Badge';
-import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
@@ -17,7 +12,7 @@ import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import { debounce } from 'es-toolkit';
 import * as monaco from 'monaco-editor';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import ThemeConfig from './components/ThemeConfig/ThemeConfig';
 import Editor from './editor/Editor';
 import { sourceAcademyEditorTheme } from './editor/theme';
@@ -42,36 +37,8 @@ export default function App() {
   });
 
   const [monarchError, setMonarchError] = useState<string | null>(null);
-
-  /**
-   * `true` when the monarch value has changed on being loaded.
-   * `false` when the monarch value hasn't changed.
-   * `resetting` when the monarch value changed due to the editor being reset.
-   */
-  const [monarchHasChanges, setMonarchHasChanges] = useState<boolean | 'resetting'>(false);
-
-  /**
-   * Set backdrop to false to hide the backdrop
-   * Set it to the language its supposed to change to to show the backdrop. If the confirm
-   * is clicked language gets changed
-   */
-  const [resetBackdrop, setResetBackdrop] = useState<false | ILanguageDefinition>(false);
-
   const monarchEditorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
-  const monarchEditorValue = monarchEditorRef.current?.getValue();
-
   const codeEditorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
-
-  useEffect(() => {
-    if (monarchEditorValue === null) return;
-
-    if (monarchHasChanges === 'resetting') {
-      setMonarchHasChanges(false);
-      return;
-    }
-
-    setMonarchHasChanges(true);
-  }, [monarchEditorValue]);
 
   const onMonarchEditorUpdate = debounce((newValue: string) => {
     localStorage.setItem('Monarch Editor', newValue);
@@ -89,24 +56,15 @@ export default function App() {
     }
   }, 200);
 
-  const beginChangeLanguageDef = (langDef: ILanguageDefinition) => {
-    if (monarchHasChanges === false) {
-      endChangeLanguageDef(langDef);
-    } else if (monarchHasChanges === true) {
-      setResetBackdrop(langDef);
-    }
-  };
-
   /**
    * Actually change the current language definition in use
    */
-  const endChangeLanguageDef = (langDef: ILanguageDefinition) => {
+  const changeLanguageDef = (langDef: ILanguageDefinition) => {
     setLanguageDef(langDef);
 
     if (typeof langDef.monarchGrammar === 'string') return;
 
     const stringified = 'return ' + stringifyMonarchGrammar(langDef.monarchGrammar);
-    setMonarchHasChanges('resetting');
     monarchEditorRef.current?.setValue(stringified);
   };
 
@@ -132,55 +90,16 @@ export default function App() {
     <Stack direction="row" sx={{ alignItems: 'center' }}>
       <Typography>Monarch Editor</Typography>
       <Tooltip title="Reset the Monarch Grammar">
-        <IconButton onClick={() => beginChangeLanguageDef(languageDef)}>
+        <IconButton onClick={() => changeLanguageDef(languageDef)}>
           <RefreshIcon />
         </IconButton>
       </Tooltip>
       <Tooltip title="Save your changes">
         <IconButton>
-          <Badge
-            invisible={monarchHasChanges !== true}
-            badgeContent='!'
-            color='primary'
-            overlap='circular'
-          >
-            <SaveIcon />
-          </Badge>
+          <SaveIcon />
         </IconButton>
       </Tooltip>
     </Stack>
-  );
-
-  const monarchChangeConfirmComponents = (
-    <Paper>
-      <div style={{ padding: '5px 5px 5px 5px'}} >
-        <Stack direction='column' spacing={1}>
-          <Typography>Are you sure you want to clear your Monarch code?</Typography>
-          <Stack
-            direction='row'
-            sx={{ justifyContent: 'center' }}
-            spacing={2}
-          >
-            <Button
-              startIcon={<DoneIcon />}
-              variant='outlined'
-              color='success'
-              onClick={() => endChangeLanguageDef(resetBackdrop as ILanguageDefinition)}
-            >
-              Yes
-            </Button>
-            <Button
-              startIcon={<CloseIcon />}
-              variant='outlined'
-              color='error'
-              onClick={() => setResetBackdrop(false)}
-            >
-              No
-            </Button>
-          </Stack>
-        </Stack>
-      </div>
-    </Paper>
   );
 
   const codeEditor = (
@@ -223,7 +142,7 @@ export default function App() {
                 size='small'
                 onChange={(_e, newLang) => {
                   if (newLang === null) return;
-                  beginChangeLanguageDef(newLang);
+                  changeLanguageDef(newLang);
                 }}
               />
             </Grid>
@@ -234,15 +153,6 @@ export default function App() {
   );
 
   return <>
-    <Backdrop
-      open={resetBackdrop !== false}
-      onClick={() => setResetBackdrop(false)}
-      sx={{
-        zIndex: 10
-      }}
-    >
-      {monarchChangeConfirmComponents}
-    </Backdrop>
     <Grid
       container
       columns={12}
